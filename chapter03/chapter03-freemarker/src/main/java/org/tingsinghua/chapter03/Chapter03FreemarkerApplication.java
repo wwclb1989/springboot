839bea97c4bddc6be1927dc342ba617794575230
wwclb <76217148@qq.com>
2019-03-24 21:36:07 +0800

    chapter03 complete

---
+++ chapter03/chapter03-freemarker/src/main/java/org/tingsinghua/chapter03/Chapter03FreemarkerApplication.java
@@ -0,0 +1,13 @@
+package org.tingsinghua.chapter03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter03FreemarkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter03FreemarkerApplication.class, args);
+	}
+
+}
---
+++ chapter03/chapter03-freemarker/src/test/java/org/tingsinghua/chapter03/Chapter03FreemarkerApplicationTests.java
@@ -0,0 +1,16 @@
+package org.tingsinghua.chapter03;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Chapter03FreemarkerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
