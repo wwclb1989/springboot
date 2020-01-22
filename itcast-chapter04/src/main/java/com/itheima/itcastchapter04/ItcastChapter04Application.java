d38776e23a42c1d1295385b12f41689ecee8db5e
wangchunbo <76217148@qq.com>
2020-01-22 16:21:34 +0800

    黑马程序员springboot第四章

---
+++ itcast-chapter04/src/main/java/com/itheima/itcastchapter04/ItcastChapter04Application.java
@@ -0,0 +1,13 @@
+package com.itheima.itcastchapter04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItcastChapter04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItcastChapter04Application.class, args);
+    }
+
+}
---
+++ itcast-chapter04/src/test/java/com/itheima/itcastchapter04/ItcastChapter04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.itcastchapter04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItcastChapter04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
