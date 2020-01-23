611c2d793158fd559fbaada6385f059595fdbfe7
wangchunbo <76217148@qq.com>
2020-01-23 14:40:26 +0800

    工程以war包方式打包

--- itcast-chapter05/src/main/java/com/itheima/itcastchapter05/ItcastChapter05Application.java
+++ itcast-chapter05/src/main/java/com/itheima/itcastchapter05/ItcastChapter05Application.java
@@ -2,13 +2,21 @@ package com.itheima.itcastchapter05;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @ServletComponentScan       // 开启基于注解方式的Servlet组件扫描支持
-public class ItcastChapter05Application {
+public class ItcastChapter05Application extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+    // 程序主类继承SpringBootServletInitializer，并重写configure()方法
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ItcastChapter05Application.class);
+    }
+
+        public static void main(String[] args) {
         SpringApplication.run(ItcastChapter05Application.class, args);
     }
 
